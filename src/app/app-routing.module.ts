83f6fe508e254c99ce2c5e878900800310b03c9f
ivaylo-mobile-dev <ivayloslav@hotmail.com>
2018-08-18 17:16:17 -0600

    added asset images for intro and logo. installed and configured capacitor for project. added a template for the home and checklist page. added ionic storage to project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/checklists', pathMatch: 'full' },
   { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
   { path: 'checklists', loadChildren: './home/home.module#HomePageModule' },
   { path: 'checklists/:id', loadChildren: './checklist/checklist.module#ChecklistPageModule' },
