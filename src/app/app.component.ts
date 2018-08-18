ce54ebc6226b0fbc2f047df6a5b7530044e29874
ivaylo-mobile-dev <ivayloslav@hotmail.com>
2018-08-17 21:46:18 -0600

    Configure capacitor, set up root component to hide SplashScreen and StatusBar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
+/*
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+*/
+
+const { SplashScreen, StatusBar } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
+  constructor() {
+    SplashScreen.hide().catch((err) => {
+      console.warn(err);
+    });
+    StatusBar.hide().catch((err) => {
+      console.warn(err);
+    });
+
+  }
+  /*constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -22,5 +36,5 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
-  }
+  }*/
 }
